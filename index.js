87201a98858f8e0c01bd2c02749679e994b231a3
Seth <stormtroopers2003@gmail.com>
2022-01-07 11:44:30 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const mainElement=document.querySelector("#main").remove();
+
+const newHeader=document.createElement('h1');
+newHeader.id="victory";
+newHeader.innerHTML="Seth is the champion";
